2e29fc0ee36f1207aa7288ec6562a555dafa6536
siren8289 <siren8289@gmail.com>
2024-08-01 16:54:16 +0900

    2. 과목 생성까지 끝

--- src/FirstTeamProject/SubjectManager.java
+++ src/FirstTeamProject/SubjectManager.java
@@ -1,14 +1,27 @@
 package FirstTeamProject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubjectManager {
-//    등록
-    public void addSubject(String id, String name, String description){
-        if(subjects.containKey(id)){
-            System.out.println("Error: Subject with ID" + id + "already exists.");
-            return;
-        }
+    //과목 이름 저장하는 *변수*
+    private String name;
+    //회차를 키로 하고 점수를 값으로 하는 점수 저장 맵
+    private Map<Integer, Integer> scores;
+    //Subject 클래스의 생성자
+    public SubjectManager(String name){
+        this.name = name; //과목 이름 초기화
+        this.scores = new HashMap<>(); //점수를 저장할 맵 초기화
     }
 
-//    추가
-//    수정
+    //Enum정의
+    public enum SubjectType{//열거형
+        REQUIRED,
+        ELECTIVE
+    }
+
+
+
+
+
 }
