0d59c6ed2aff205aa5ef90df3e5c6cb9caa9e23f
Tema_Project-1- <dnjsdyd78@gmail.com>
2024-08-01 15:00:55 +0900

    코드 옮기고 커밋

--- src/FirstTeamProject/Student.java
+++ src/FirstTeamProject/Student.java
@@ -1,4 +1,32 @@
 package FirstTeamProject;
 
-public class Student {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Student{
+    private Map<String, String> StudentInfoGroup = new HashMap<>();
+    private String StudentName;
+    private String UniqueId;
+    private String Subject;
+
+
+
+    public void infoSet(String StudentName, String UniqueId){
+        this.StudentName = StudentName;
+        this.UniqueId = UniqueId;
+        StudentInfoGroup.put(this.UniqueId, this.StudentName);
+        StudentInfoGroup.put(this.StudentName, this.UniqueId);
+
+    }
+    public String getStudentName(String UniqueId){
+        return StudentInfoGroup.get(UniqueId);
+    }
+
+    public String getUniqueId(String StudentName){
+        return StudentInfoGroup.get(StudentName);
+    }
+
+
+
+
 }
